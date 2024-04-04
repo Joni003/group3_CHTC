5e16b277a580c68dca163e4a27efcc9e30e1b9ce
Eugene <ohba@wisc.edu>
2024-04-04 12:51:40 -0500

    I did it boys

--- pre.sh
+++ pre.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#Download shakespeare with wget
+# Download Shakespeare archive with wget
+wget https://pages.stat.wisc.edu/~jgillett/DSCP/CHTC/wordCounting/shakespeare.tar
 
-wget https://pages.stat.wisc.edu/~jgillett/DSCP/CHTC/wordCounting/shakespear.tar
+# Extract the Shakespeare archive
+tar -xf shakespeare.tar
 
-#Tar
-tar -xf shakespear.tar
-
-#Concatenate all files
+# Concatenate all play files into one large file
 cat */* > all_plays.txt
 
-#Split
+# Split the large file into 5 smaller files
 split -n 5 all_plays.txt output_version
