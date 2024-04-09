ef222828db8be82d7d50b1122522815b988bb2b7
jzhang2449 <jzhang2449@learn.chtc.wisc.edu>
2024-04-09 11:53:19 -0500

    changed output folder to preoutput

--- pre.sh
+++ pre.sh
@@ -14,6 +14,6 @@ mkdir output
 cat */*/* > all_plays.txt
 
 # Split the large file into 5 smaller files
-split -n 5 all_plays.txt output/output_version
+split -n 5 all_plays.txt preoutput/output_version
 
 ls -1 output/output_version* > InputFileList
